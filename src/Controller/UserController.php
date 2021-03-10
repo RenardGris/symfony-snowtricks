ce0944e11cc9c0677bca0a6dfa5c816ec2b64c52
RenardGris <73963127+RenardGris@users.noreply.github.com>
2021-03-10 19:38:46 +0100

    Register : get forms and save new user when send for verification

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
@@ -34,14 +39,37 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/register", name="register", methods={"GET"})
+     * @Route("/register", name="register")
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param UserPasswordEncoderInterface $encoder
      * @return Response
      *
      */
-    public function register(): Response
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+
+            $user->setPassword($hash);
+            $user->setAvatar('http://placeimg.com/120/120/any')
+                ->setValidate(false)
+                ->setToken(md5($user->getEmail()));
+
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render('user/register.html.twig', [
+            'formUserRegistration' => $form->createView()
         ]);
     }
 
