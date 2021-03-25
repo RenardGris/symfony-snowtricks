c161e9d4160eeb2b932aa82977b0ec1fc779c3a4
RenardGris <73963127+RenardGris@users.noreply.github.com>
2021-03-25 04:16:33 +0100

    Add : limit_per_page

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -14,6 +14,8 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class CommentRepository extends ServiceEntityRepository
 {
+    const LIMIT_PER_PAGE = 10;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Comment::class);
