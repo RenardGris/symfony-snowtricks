eddb4dc4e0f0c6020861fb10163f50822d8014d9
RenardGris <73963127+RenardGris@users.noreply.github.com>
2021-03-27 23:40:13 +0100

    Refacto : set video as collection

--- src/Form/FigureType.php
+++ src/Form/FigureType.php
@@ -4,7 +4,9 @@ namespace App\Form;
 
 use App\Entity\Figure;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -21,11 +23,19 @@ class FigureType extends AbstractType
                 'mapped' => false,
                 'required' => false
             ])
-            ->add('videos', FileType::class, [
-                'multiple' => true,
+            ->add('videos', CollectionType::class, [
+                'required' => false,
+                'entry_type' => TextType::class,
+                'entry_options' =>
+                    [
+                        'attr' => ['class' => 'col-12']
+                    ],
+                'allow_add' => true,
+                'allow_delete' => true,
+                'prototype' => true,
                 'mapped' => false,
-                'required' => false
-            ])
+                'label' => false,
+            ]);
         ;
     }
 
